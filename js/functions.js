5a383e89972141b82bd255faf60fa437d80d45bd
rahulbhatt <rahul@dswtpl.com>
2013-12-17 18:23:07 +0530

    commit

--- js/functions.js
+++ js/functions.js
@@ -2,18 +2,30 @@ $("#register").click(function(){
 	var Email = $('#Email').val();
 	var Password = $('#Password').val();
 	var ConfirmPassword = $('#ConfirmPassword').val();
+	var data = { 'Email': Email, 'Password': Password };
+	/*$('form').submit(function(){
+	var landmarkID = $(this).parent().attr('data-landmark-id');
+	var postData = $(this).serialize();
 	
 	$.ajax({
 		type: 'POST',
-		data: '&Email='+Email+'&Password='+Password,
-		url: serviceURL + 'mobile/register.php',
+		data: postData+'&amp;lid='+landmarkID,
+		url: 'http://your-domain.com/comments/save.php',
 		success: function(data){
 			console.log(data);
 			alert('Your comment was successfully added');
 		},
 		error: function(){
+			console.log(data);
 			alert('There was an error adding your comment');
 		}
+	});
+	
+	return false;
+});*/
+
+	$.getJSON(serviceURL + 'mobile/register.php', function(res) {
+		alert(res);
 	});
 	
 });
