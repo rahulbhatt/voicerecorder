64a6733753ef24af01299b9945a1b7f21613e85d
rahulbhatt <rahul@dswtpl.com>
2013-12-23 12:46:09 +0530

    test

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var serviceURL = "http://180.211.104.123/www/emp1/speakpipe/";
+var serviceURL = "http://redesignbox.com/speakpipe/";
 $( document ).bind( "mobileinit", function() {
     $.mobile.allowCrossDomainPages = true;
 });
