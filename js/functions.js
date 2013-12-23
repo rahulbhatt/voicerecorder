5b2c5c8c2b0e93071768c387cefdc0fde934d3f9
rahulbhatt <rahul@dswtpl.com>
2013-12-23 14:22:25 +0530

    s

--- js/functions.js
+++ js/functions.js
@@ -5,30 +5,27 @@ function register(){
 	var Email = $('#Email').val();
 	var Password = $('#Password').val();
 	var ConfirmPassword = $('#ConfirmPassword').val();
-	alert(serviceURL);
-	$.getJSON(serviceURL + 'mobile/register.php?Email='+Email+'&Password='+Password, function(data) {
-			$('#Email').val("");
-			$('#Password').val("");
-			$('#ConfirmPassword').val("");
-	});
-}
-$("#register").click(function(){
-	
-	/*$.ajax({
+	alert(serviceURL + 'mobile/register.php');
+	$.ajax({
 		async: false,
 		type: 'POST',
 		data: '&Email='+Email+'&Password='+Password,
 		url: serviceURL + 'mobile/register.php',
 		dataType: "json",
 		success: function(data){
-			$('#mybtns').html(btn);
+			//$('#mybtns').html(btn);
 			$('#Email').val("");
 			$('#Password').val("");
-			$('#ConfirmPassword').val("");
-			$('#errorMessage').html("You have successfully registered with us. Please check your email.");
+			//$('#ConfirmPassword').val("");
+			//$('#errorMessage').html("You have successfully registered with us. Please check your email.");
 		},
 		error: function(){
 		}
-	});*/
+	});
+	
+}
+$("#register").click(function(){
+	
+	/**/
 
 });
